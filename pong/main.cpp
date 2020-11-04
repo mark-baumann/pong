758a9dfe61286fb9b504f028b8cc86f822d29764
Mark Baumann <57982395+marky-mark-88@users.noreply.github.com>
2020-11-04 17:51:17 +0100

    pong windows

---
+++ pong/main.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <Windows.h>
+
+
+using namespace std;
+
+
+
+	
+
+	int gamespeed = 75;
+
+	int balldir =2;
+	int mapwidth=50;
+	int maphigh=30;
+	int ballX = mapwidth / 2;
+	int ballY = maphigh / 2;
+
+	int spieler1x = 5;
+	int spieler1y = maphigh / 2;
+	int spieler2y = maphigh / 2;
+	int spieler2x = mapwidth - 5;
+
+void player1move() {
+
+	if(GetKeyState('W') & 0x800) {
+		spieler1y--;
+
+	}
+
+	if(GetKeyState('S') & 0x800) {
+		spieler1y++;
+	}
+}
+
+void player2move() {
+	if(GetKeyState('I') & 0x800) {
+		spieler2y--;
+
+	}
+
+	if(GetKeyState('K') & 0x800) {
+		spieler2y++;
+	}
+
+}
+
+void ballLogik() {
+
+	if (balldir == 2) {
+		ballX--;
+	}
+
+	if (balldir == 1) {
+		ballX--;
+		ballY--;
+	}
+
+	if (balldir == 3) {
+		ballX--;
+		ballY++;
+
+	}
+
+	if (balldir == 4) {
+		ballX++;
+		ballY--;
+	}
+
+	if (balldir == 5) {
+		ballX++;
+
+	}
+
+	if (balldir == 6) {
+		ballX++;
+		ballY++;
+	}
+
+
+	
+		if (ballX == spieler1x && ballY == spieler1y-2) {
+			
+			balldir = 4;
+			
+		}
+		if (ballX == spieler1x && ballY == spieler1y-1) {
+			
+			balldir = 5;
+			
+		}
+		if (ballX == spieler1x && ballY == spieler1y) {
+			
+			balldir = 6;
+			
+		}
+
+
+
+	
+}
+
+bool endgame = false;
+
+
+void map(){
+
+	for (int i=0; i<=maphigh; i++) {
+		for(int j=0; j<=mapwidth; j++) {
+			
+			if (i==0 || i == maphigh ) {
+				cout << "#";
+			}
+
+			else if (j==0 || j == mapwidth ) {
+				cout << "#";
+			}
+			else if (spieler1x == j && spieler1y == i)
+			{
+				cout << "p";
+			}
+			else if (spieler1x == j && spieler1y-1 == i)
+			{
+				cout << "p";
+			}
+			else if (spieler1x == j && spieler1y -2 == i)
+			{
+				cout << "p";
+			}
+			else if (spieler2x == j && spieler2y == i) {
+				cout<< "x";
+			}
+			else if (spieler2x == j && spieler2y-1 == i) {
+				cout<< "x";
+			}
+			else if (spieler2x == j && spieler2y-2 == i) {
+				cout<< "x";
+			}
+
+			else if(ballX == j && ballY == i) {
+
+				cout<<"o";
+			}
+
+			else {
+				cout << " ";
+			}
+			
+
+
+		}
+			cout<<endl;
+	}
+}
+
+int main() {
+	
+
+	
+	while(!endgame){
+		map();
+		player1move();
+		player2move();
+		ballLogik();
+		Sleep(gamespeed);
+		system("cls");
+	}
+
+
+	system("pause");
+
+}
